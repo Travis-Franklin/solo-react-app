24740e75bf1b3b831e27f70882d75c9612cea0e3
Travis Franklin <travis.william.franklin@gmail.com>
2020-02-19 14:27:10 -0500

    updates app

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
   return (
     <div className="App">
       <h1>Take Me to Your Leader</h1>
+      <h2>A leaderboard to track who is up and who is down. 
+        Made for Office Pools, Friendly Competitions and 
+        Presidential Primaries.</h2>
       <HowManyToWin
       />
       {/* <Counter/>
